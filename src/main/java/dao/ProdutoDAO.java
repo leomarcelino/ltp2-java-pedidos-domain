ddb61835727ab0862657149e26fd5d7575ab3e8e
Leonardo Marcelino <leonardo.marcelino@gmail.com>
2019-10-28 22:12:23 -0300

    Finalizar implementação

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -1,8 +1,5 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -11,11 +8,7 @@ import java.util.List;
 import factories.ConnectionFactory;
 import models.Produto;
 
-public class ProdutoDAO {
-
-	private Connection conn;
-	private PreparedStatement pst;
-	private ResultSet rs;
+public class ProdutoDAO extends BaseDAO {
 	
 	public Produto create(Produto produto) {
 		String sql = "INSERT INTO produtos(descricao, preco, estoque)"
@@ -59,11 +52,7 @@ public class ProdutoDAO {
 			rs = pst.executeQuery();
 			
 			if (rs.next()) {
-				produto = new Produto();
-				produto.setId(rs.getLong("id"));
-				produto.setDescricao(rs.getString("descricao"));
-				produto.setPreco(rs.getDouble("preco"));
-				produto.setEstoque(rs.getDouble("estoque"));
+				produto = readFieldValues();
 			}
 		} catch(SQLException e) {
 			throw new RuntimeException("Error in finding record", e);
@@ -76,40 +65,93 @@ public class ProdutoDAO {
 		return produto;
 	}
 
+	private Produto readFieldValues() throws SQLException {
+		Produto produto;
+		produto = new Produto();
+		produto.setId(rs.getLong("id"));
+		produto.setDescricao(rs.getString("descricao"));
+		produto.setPreco(rs.getDouble("preco"));
+		produto.setEstoque(rs.getDouble("estoque"));
+		
+		return produto;
+	}
+
 	
-	public List<Produto> list() {
-		return new ArrayList<Produto>();
+	public List<Produto> list(String termo) {
+		String sql = "SELECT id, descricao, preco, estoque"
+				+ " FROM produtos";
+		
+		if (termo != null && !termo.isEmpty()) {
+			sql += " WHERE descricao like ?";
+		}
+		
+		List<Produto> lista = new ArrayList<>();
+		
+		try {
+			conn = ConnectionFactory.getInstance().getConnection();
+			pst = conn.prepareStatement(sql);
+			
+			if (termo != null && !termo.isEmpty()) {
+				pst.setString(1, '%' + termo + '%');
+			}
+			
+			rs = pst.executeQuery();
+			
+			while (rs.next()) {
+				Produto produto = readFieldValues();
+				
+				lista.add(produto);
+			}
+		} catch(SQLException e) {
+			throw new RuntimeException("Erro ao ler registros");
+		}
+		
+		close(rs);
+		close(pst);
+		close(conn);
+				
+		return lista;
 	}
 	
 	public Produto update(Produto produto) {
+		String sql = "UPDATE produtos SET "
+				+ " descricao = ?,"
+				+ " preco = ?,"
+				+ " estoque = ?"
+				+ " WHERE id = ?";
+		
+		try {
+			conn = ConnectionFactory.getInstance().getConnection();
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, produto.getDescricao());
+			pst.setDouble(2, produto.getPreco());
+			pst.setDouble(3, produto.getEstoque());
+			pst.setLong(4, produto.getId());
+			pst.executeUpdate();
+		} catch(SQLException e) {
+			throw new RuntimeException("Erro ao atualizar registro");
+		}
+		
+		close(pst);
+		close(conn);
+		
 		return produto;
 	}
 	
 	public void delete(Produto produto) {
+		String sql = "DELETE FROM produtos "
+				+ " WHERE id = ?";
 		
-	}
-	
-	private void close(Connection conn) {
-		if (conn != null) {
-			try {
-				conn.close();
-			} catch(Exception ignored) {}
+		try {
+			conn = ConnectionFactory.getInstance().getConnection();
+			pst = conn.prepareStatement(sql);
+			pst.setLong(1, produto.getId());
+			pst.executeUpdate();
+		} catch(SQLException e) {
+			throw new RuntimeException("Erro ao excluir registro");
 		}
-	}
-
-	private void close(Statement st) {
-		if (st != null) {
-			try {
-				st.close();
-			} catch(Exception ignored) {}
-		}
-	}
-	
-	private void close(ResultSet rs) {
-		if (rs != null) {
-			try {
-				rs.close();
-			} catch(Exception ignored) {}
-		}
+		
+		close(pst);
+		close(conn);
 	}
 }
