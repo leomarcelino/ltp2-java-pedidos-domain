ae9f5e9e7c372bf35501e06238afcfffc1a31c49
Leonardo Marcelino <leonardo.marcelino@gmail.com>
2019-10-28 22:12:36 -0300

    Criar estrutura do DAO

---
+++ src/main/java/dao/ClienteDAO.java
@@ -0,0 +1,49 @@
+package dao;
+
+import java.util.List;
+
+import models.Cliente;
+
+public class ClienteDAO extends BaseDAO {
+	
+	public Cliente create(Cliente cliente) {
+		/*
+		 * TODO: Implementar logica
+		 */
+		
+		return cliente;
+	}
+	
+	public Cliente find(long id) {
+		/*
+		 * TODO: Implementar logica
+		 */
+		
+		return null;
+	}
+	
+	public List<Cliente> list(String termo) {
+		/*
+		 * TODO: Implementar logica
+		 */
+		
+		return null;
+	}
+	
+	public Cliente update(Cliente cliente) {
+		/*
+		 * TODO: Implementar logica
+		 */
+		
+		return cliente;
+	}
+	
+	
+	public void delete(Cliente cliente) {
+		/*
+		 * TODO: Implementar logica
+		 */
+		
+	}
+	
+}
